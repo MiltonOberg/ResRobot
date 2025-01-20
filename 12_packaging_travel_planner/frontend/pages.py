52c21df801028fd7d2685d9e679040238470c07d
Milton Österberg <miltonwendel@outlook.com>
2025-01-20 13:11:39 +0100

    pages and menu on dashboard

---
+++ 12_packaging_travel_planner/frontend/pages.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from plot_maps import TripMap
+from utils.constants import StationIds
+
+def home():
+    st.markdown("# Startsida")
+
+def reseplanerare():
+    st.markdown("# Reseplanerare")
+    st.markdown(
+        "Den här dashboarden syftar till att både utforska data för olika platser, men ska även fungera som en reseplanerare där du får välja och planera din resa."
+    )
+    trip_map = TripMap(
+        origin_id=StationIds.MALMO.value, destination_id=StationIds.UMEA.value
+    )
+    trip_map.display_map()
