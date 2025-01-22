7e45c11d649ef80a50a0f063e3b9f6a5f0b3b931
Sara Mellgren <143341964+saraonsala@users.noreply.github.com>
2025-01-22 11:23:15 +0100

    class DepaturePlanner done

--- 12_packaging_travel_planner/backend/departures.py
+++ 12_packaging_travel_planner/backend/departures.py
@@ -1,14 +1,12 @@
-from backend.connect_to_api import ResRobot
 from trips import TripPlanner
-from connect_to_api import 
-
-
+from connect_to_api import ResRobot
+import pandas as pd
+import requests
 
 resrobot = ResRobot()
 origin_id = input("Enter your location: ")
-destination_id = input("Enter your destination: ")
-
-
+destination_id = input("Enter your destination: ")  
+ 
 class DepaturePlanner(TripPlanner):
     
     def __init__(self, origin_id, destination_id):
@@ -16,14 +14,13 @@ class DepaturePlanner(TripPlanner):
         self.origin_id = origin_id
         self.destination_id = destination_id
         self.trips = resrobot.trips(self.origin_id, self.destination)
-        
-                
+                  
     def next_available_trip(self) -> pd.DataFrame:
         next_trip = self.trips[0]
         return next_trip
        
     def timetable_departure(self, location_id):
-        location_id = origin_id
+        
         url = f"https://api.resrobot.se/v2.1/departureBoard?id={location_id}&format=json&accessId={self.API_KEY}"
         location_id = origin_id
         response = requests.get(url)
