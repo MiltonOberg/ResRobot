b5d7c027693670627393aa0f62c887774109d183
Sara Mellgren <143341964+saraonsala@users.noreply.github.com>
2025-01-27 17:20:58 +0100

    clean

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -2,6 +2,7 @@ from frontend.pages_dict import page_option
 import streamlit as st
 import pathlib
 
+
 def load_css(file_path):
     with open(file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
@@ -14,7 +15,7 @@ load_css(css_path)
 
 st.markdown('<h1 class="title-text">✨ Welcome to our Travel Robot Dashboard ✨</h1>', unsafe_allow_html=True)
 st.markdown('<h2 class="subheader-text">Your complete travel guide 🚆 🚋 🚌</h2>', unsafe_allow_html=True)
-st.markdown('<p class="description-text">This dashboard is designed to help you plan your next trip, explore different locations, and view the latest travel data.</p>', unsafe_allow_html=True)    
+    
 st.display_map()
 st.divider()
 
@@ -41,18 +42,19 @@ st.markdown(
     '<p class="custom-markdown">This is <strong>bold text</strong> with a custom font and color.</p>',
     unsafe_allow_html=True,
 )
+
+
 def layout():
     st.sidebar.title("🚀 Navigation")
-    st.sidebar.markdown("Select a page to view:")
-    st.sidebar.markdown("---")
     st.sidebar.radio("Go to", list(page_option.keys()))
- 
+
      
 # Skapar en connection till databasen
 def main():
     layout()  # Visa huvudlayouten
     page = st.sidebar.radio("# Sidor", page_option.keys())  # Lägg till sidopanel
     page_option[page]()  # Kör funktionen kopplad till den valda sidan
+    
 
 if __name__ == "__main__":
     main()  # Kör huvudfunktionen
