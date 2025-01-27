631fd816ad5c702f1e0d59910a1bcf6e795847d4
Sara Mellgren <143341964+saraonsala@users.noreply.github.com>
2025-01-27 08:25:40 +0100

    fixed background-image

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -1,11 +1,36 @@
-import streamlit as st
 from frontend.pages_dict import page_option
+import streamlit as st 
+import plotly.express as px
+import pandas as pd
+import sqlite3
 
+# CSS för att sätta bakgrundsbild
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://www.saramellgren.com/wp-content/uploads/2023/11/saram0214_an_astronaut_playing_with_jellyfish_fa601ffe-ecda-4a23-9977-f958ecb739e4-1.png");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
+def layout():
+    st.title("Welcome to our Travel Robot Dashboard")
+    st.markdown("Your complete travel guide")
+    st.markdown("---")
+    
+# Skapar en connection till databasen
 def main():
-    page = st.sidebar.radio("# Sidor", page_option.keys())
-    page_option[page]()
+    layout()  # Visa huvudlayouten
+    page = st.sidebar.radio("# Sidor", page_option.keys())  # Lägg till sidopanel
+    page_option[page]()  # Kör funktionen kopplad till den valda sidan
 
 
 if __name__ == "__main__":
-    main()
+    main()  # Kör huvudfunktionen
