0d41c53cea715fe70637b2113425c82780e5f905
KevinKinnunen <54663592+KevinKinnunen@users.noreply.github.com>
2025-02-04 19:26:24 +0100

    Visuella förbättringar i UI (#67)
    - Lagt till svart bakgrund på varje sida för bättre kontrast.
    - Ändrat fonten.
    - Justerat textfärger (vit text) för bättre läsbarhet.

--- frontend/dashboard_pages/home_page.py
+++ frontend/dashboard_pages/home_page.py
@@ -2,11 +2,13 @@ import streamlit as st
 
 
 def home():
+    st.markdown('<div class="start-container">', unsafe_allow_html=True)
     st.markdown(
-        '<h1 class="title-text">✨ Welcome to our Travel Robot Dashboard ✨</h1>',
+        '<h2 class="title-text">✨Welcome to our Travel Robot✨</h2>',
         unsafe_allow_html=True,
     )
     st.markdown(
-        '<h2 class="subheader-text">Your complete travel guide 🚆 🚋 🚌</h2>',
+        '<p class="loremipsum-text">Vivamus vel sollicitudin risus. Cras vel quam rhoncus, efficitur tellus a, luctus diam. Curabitur efficitur odio diam, lobortis pharetra purus tempor eu. Fusce id semper nibh, at congue orci. Vestibulum non efficitur lacus, id suscipit nunc. Nulla imperdiet mattis feugiat. Pellentesque elit nisi, vestibulum nec risus quis, ornare congue ligula. Quisque sollicitudin arcu non ipsum bibendum interdum. Morbi non porttitor nunc, vel ultrices ligula. Suspendisse potenti. Aenean placerat ligula dolor, sit amet molestie est ultricies sed. Vivamus enim lorem, mattis ut rhoncus in, pretium eget dui. Quisque dictum enim nec turpis pretium sollicitudin. </p>',
         unsafe_allow_html=True,
     )
+    st.markdown("</div>", unsafe_allow_html=True)
