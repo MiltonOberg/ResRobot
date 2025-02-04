917cde2bc86aa72771a610f5eccc6adca9a8a316
KevinKinnunen <54663592+KevinKinnunen@users.noreply.github.com>
2025-02-04 19:41:00 +0100

    Update home_page.py

--- frontend/dashboard_pages/home_page.py
+++ frontend/dashboard_pages/home_page.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+lorem_ipsum_temp = "Vivamus vel sollicitudin risus. Cras vel quam rhoncus, efficitur tellus a, luctus diam. Curabitur efficitur odio diam, lobortis pharetra purus tempor eu. Fusce id semper nibh, at congue orci. Vestibulum non efficitur lacus, id suscipit nunc. Nulla imperdiet mattis feugiat. Pellentesque elit nisi, vestibulum nec risus quis, ornare congue ligula. Quisque sollicitudin arcu non ipsum bibendum interdum. Morbi non porttitor nunc, vel ultrices ligula. Suspendisse potenti. Aenean placerat ligula dolor, sit amet molestie est ultricies sed. Vivamus enim lorem, mattis ut rhoncus in, pretium eget dui. Quisque dictum enim nec turpis pretium sollicitudin. "
+
 
 def home():
     st.markdown('<div class="start-container">', unsafe_allow_html=True)
@@ -8,7 +10,7 @@ def home():
         unsafe_allow_html=True,
     )
     st.markdown(
-        '<p class="loremipsum-text">Vivamus vel sollicitudin risus. Cras vel quam rhoncus, efficitur tellus a, luctus diam. Curabitur efficitur odio diam, lobortis pharetra purus tempor eu. Fusce id semper nibh, at congue orci. Vestibulum non efficitur lacus, id suscipit nunc. Nulla imperdiet mattis feugiat. Pellentesque elit nisi, vestibulum nec risus quis, ornare congue ligula. Quisque sollicitudin arcu non ipsum bibendum interdum. Morbi non porttitor nunc, vel ultrices ligula. Suspendisse potenti. Aenean placerat ligula dolor, sit amet molestie est ultricies sed. Vivamus enim lorem, mattis ut rhoncus in, pretium eget dui. Quisque dictum enim nec turpis pretium sollicitudin. </p>',
+        f'<p class="loremipsum-text">{lorem_ipsum_temp}</p>',
         unsafe_allow_html=True,
     )
     st.markdown("</div>", unsafe_allow_html=True)
